ee0db81b8dc0fa8091e4fbbccf0ded5b100246a2
Nisuga Jayawardana <nisuga.rockwell@gmail.com>
2020-10-19 02:07:42 +0530

    final

--- utils/db_Connection.js
+++ utils/db_Connection.js
@@ -3,7 +3,5 @@ module.exports = {
     "mongodb+srv://admin:admin@procurementsystem.btw0b.mongodb.net/ProcurementDB?retryWrites=true&w=majority",
 };
 
-// username:     procuracsseproject@gmail.com
-// password:     csseproject
 
 
